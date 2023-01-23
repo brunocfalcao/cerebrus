697aa67f636062607ead21993b925b5e5e85eeaa
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-01-23 18:48:40 +0100

    wip

---
+++ src/Cerebrus.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Brunocfalcao\Cerebrus;
+
+class Cerebrus
+{
+    private static $_instance = null;
+
+    public const PHP_SESSION_DISABLED = 'SESSION_DISABLED';
+
+    public const PHP_SESSION_NONE = 'SESSION_NONE';
+
+    public const PHP_SESSION_ACTIVE = 'SESSION_ACTIVE';
+
+    public function __construct(string $path = null)
+    {
+        $status = session_status();
+
+        switch ($status) {
+            case PHP_SESSION_DISABLED:
+                throw new \Exception('Your web app cannot use sessions. Cerebrus aborted');
+                break;
+
+            case PHP_SESSION_NONE:
+                $this->start($path);
+                break;
+        }
+    }
+
+    public function has(string $key): bool
+    {
+        return isset($_SESSION[$key]);
+    }
+
+    public function get(string $key)
+    {
+        return isset($_SESSION[$key]) ? $_SESSION[$key] : null;
+    }
+
+    public function all(): ?array
+    {
+        return $_SESSION;
+    }
+
+    public function set(string $key, mixed $value): void
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public function unset(string $key): void
+    {
+        if (array_key_exists($key, $_SESSION)) {
+            unset($_SESSION[$key]);
+        };
+    }
+
+    public function destroy(): void
+    {
+        session_destroy();
+    }
+
+    public function getStatus(): int
+    {
+        return session_status();
+    }
+
+    public function getId(): string
+    {
+        return session_id();
+    }
+
+    private function start(string|null $path = 'tmp'): void
+    {
+        try {
+            session_start();
+        } catch (\Exception $e) {
+            /**
+             * Mostly a file write directory exception.
+             * Creates and set the session writable directory path
+             * in the /tmp on your Laravel project base path.
+             */
+            if (! is_dir(session_save_path())) {
+                $path = $path ?? base_path('tmp');
+                @mkdir($path);
+                session_save_path($path);
+            }
+
+            session_start();
+        }
+    }
+}
---
+++ src/ConcernsSessionPersistence.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Brunocfalcao\Cerebrus;
+
+trait ConcernsSessionPersistence
+{
+    /**
+     * The session prefix (without the session id suffix). Normally defined
+     * once on the class constructor, or directly in the attribute assignment.
+     *
+     * @var string
+     */
+    protected $prefix;
+
+    /**
+     * Sets or gets the current session key (or passed key). You can pass
+     * another key prefix in case you are using this method on the same class
+     * but for a different prefix than the one assigned via the $this->prefix
+     * attribute.
+     *
+     * @param  callable    $callable The returned session value callable.
+     *
+     * @return this
+     */
+    public function persist(callable $callable)
+    {
+        $this->checkRequirements();
+
+        $session = new Cerebrus();
+
+        // Do we already have a session key? -- Just return it.
+        if ($session->has($this->key())) {
+            return $this;
+        }
+
+        // Compute callable and store it in session.
+        $session->set($this->key(), $this->compute($callable));
+
+        return $this;
+    }
+
+    /**
+     * Returns the possible session key for this session instance.
+     *
+     * @return mixed
+     */
+    public function obtain()
+    {
+        $this->checkRequirements();
+
+        return $this->session();
+    }
+
+    /**
+     * Overwrites a session key, or writes for the first time.
+     *
+     * @param  callable $callable The returned session value callable.
+     *
+     * @return this
+     */
+    public function overwrite(callable $callable)
+    {
+        $this->checkRequirements();
+
+        // Compute callable and store/overwrite it in session.
+        (new Cerebrus())->set($this->key(), $this->compute($callable));
+
+        return $this;
+    }
+
+    public function invalidateIf(callable $callable, $invalidateEmptys = true)
+    {
+        $this->checkRequirements();
+
+        $session = new Cerebrus();
+
+        /**
+         * If the session key exists and it's null we should invalidate it
+         * too, in case $invalidateEmptys too.
+         */
+        if (really_empty($session->get($this->key())) && $invalidateEmptys) {
+            $session->unset($this->key());
+        }
+
+        if ($callable() === true) {
+            $session->unset($this->key());
+        }
+
+        return $this;
+    }
+
+    /**
+     * Sets the session prefix to be used on the next calls.
+     *
+     * @param  string  $prefix Session prefix
+     * @return $this
+     */
+    public function withPrefix(string $prefix)
+    {
+        $this->prefix = $prefix;
+
+        return $this;
+    }
+
+    /**
+     * Returns the session value in case the session is valid. If not, then
+     * assesses the session and returns the value.
+     *
+     * @return mixed
+     */
+    protected function session()
+    {
+        $this->checkRequirements();
+
+        return (new Cerebrus())->get($this->key());
+    }
+
+    public function sessionId()
+    {
+        return (new Cerebrus())->getId();
+    }
+
+    /**
+     * Returns the computed key given a prefix.
+     *
+     * @return string
+     */
+    protected function key()
+    {
+        $this->checkRequirements();
+
+        return $this->prefix.':'.(new Cerebrus())->getId();
+    }
+
+    /**
+     * Computes a callable (normally used for the persist() and overwrite()).
+     *
+     * @param  callable $callable
+     *
+     * @return mixed
+     */
+    protected function compute(callable $callable)
+    {
+        return $callable();
+    }
+
+    protected function checkRequirements()
+    {
+        if (blank($this->prefix)) {
+            throw new \Exception("No key defined for the session persistence instance");
+        }
+    }
+}
